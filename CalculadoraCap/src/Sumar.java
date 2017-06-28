a0e405ab5acfa0047437a5d18ef5e3ec4da85831
Gonzalez Albañil <daniel.gonzalez-albanil@capgemini.com>
2017-06-28 12:34:57 +0200

    clase sumar

--- CalculadoraCap/src/Sumar.java
+++ CalculadoraCap/src/Sumar.java
@@ -3,8 +3,20 @@
  * @author dgonza16
  *
  */
-public class Sumar {
+public class Sumar 
+{
+  double resultado; //variable para almacenar el resultado de la suma
 
-  int n1, n2;
+  public static void main(String[] args) 
+  {
+    //metodo que recibe dos numeros y devuelve la suma de ambos
+      public double Suma(int n1,int n2)
+      {
+        resultado = n1 + n2; //realizamos la operación de suma
+        
+        return resultado; //devolvemos el resultado de la suma
+      }
+
+  }
 
 }
