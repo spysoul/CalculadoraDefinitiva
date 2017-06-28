fecaa0d8f41bfb6848b303ef2a6bae86f220c8b2
alejandrrmov75 <nometoquesloshuevazos@gmail.com>
2017-06-28 12:54:06 +0200

    Add files via upload

---
+++ calculadorafin/calculadorafinalisima/src/calculadorafinalisima/Main.java
@@ -0,0 +1,26 @@
+package calculadorafinalisima;
+
+import java.util.Scanner;
+
+/**
+ * @author arodrigu
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    int num1, num2;
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.print("Mete numero1");
+    num1 = teclado.nextInt();
+    System.out.print("Mete numero1");
+    num2 = teclado.nextInt();
+
+  }
+
+}
