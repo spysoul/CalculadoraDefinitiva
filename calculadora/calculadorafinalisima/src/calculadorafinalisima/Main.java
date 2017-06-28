bc6c5320bbe82ffb149c2010285afa6b079bdb89
Gonzalez Albañil <daniel.gonzalez-albanil@capgemini.com>
2017-06-28 12:55:06 +0200

    hola

--- calculadora/calculadorafinalisima/src/calculadorafinalisima/Main.java
+++ calculadora/calculadorafinalisima/src/calculadorafinalisima/Main.java
@@ -4,14 +4,26 @@ package calculadorafinalisima;
  * @author arodrigu
  *
  */
-public class Main {
+public class Main 
+{
 
   /**
    * @param args
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     // TODO Auto-generated method stub
 
+  }
+  
+  //Metodo que recibe dos numeros y devuleve la suma de ambos
+  Public double Suma (double n1, double n2)
+  {
+    double resultado; //variable para almacenar el resultado
+    
+    resultado = n1 + n2; //operacion de suma
+    
+    return resultado; //devolvemos el resultado
   }
 
 }
